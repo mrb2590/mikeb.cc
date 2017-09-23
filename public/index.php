8f216f307824c472d87d18ba216d2ccfb168358d
mrb2590@gmail.com <mrb2590@gmail.com>
2017-09-23 16:28:13 -0400

    Added title to page

--- public/index.php
+++ public/index.php
@@ -1,10 +1,16 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+<title>mikeb.cc</title>
 <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <style>
+	html, body {
+		margin: 0;
+		padding: 0;
+		text-align: center;
+	}
 	div {
 		margin: 15px 0;
 	}
